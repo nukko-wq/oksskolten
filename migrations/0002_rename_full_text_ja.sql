05b5fd8ed935056e6c8fcbad3ba38f0aa9462b41
babarot <b4b4r07@gmail.com>
2026-03-12 19:43:22 +0900

    Squash translation column migrations into initial schema
    Fold full_text_ja → full_text_translated rename and translated_lang
    addition directly into 0001_initial.sql since the project has not
    been published yet. Remove now-redundant 0002 and 0003 migrations.

--- migrations/0002_rename_full_text_ja.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE articles RENAME COLUMN full_text_ja TO full_text_translated;
-ALTER TABLE articles ADD COLUMN translated_lang TEXT;
--- migrations/0003_backfill_translated_lang.sql
+++
@@ -1,1 +0,0 @@
-UPDATE articles SET translated_lang = 'ja' WHERE full_text_translated IS NOT NULL AND translated_lang IS NULL;
